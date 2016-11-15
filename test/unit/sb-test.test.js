46a8c71fbce0e744106d5a20e399000927e85c9c
brandonocasey <brandonocasey@gmail.com>
2016-11-15 16:40:02 -0500

    changed run all to run parallel added run series added lint during test run, but only fail after finished updated tests to not run main binaries to save time

--- test/unit/sb-test.test.js
+++ test/unit/sb-test.test.js
@@ -6,12 +6,42 @@ var PathsExist = require('../../src/utils/paths-exist');
 var binName = 'sb-clean';
 var parallel = require('mocha.parallel');
 
-['sb-test-all', 'sb-test', 'sb-test-browser'].forEach(function(binName) {
+['sb-test-all', 'sb-test-browser'].forEach(function(binName) {
   parallel(binName, function() {
-    it('should run without error', function(done) {
-      var helper = new TestHelper();
+    it('should run error on linter', function(done) {
+      var helper = new TestHelper({copyDist: true});
 
-      helper.exec(binName, function(code, stdout, stderr) {
+      helper.exec(binName, ['--no-build'], function(code, stdout, stderr) {
+
+        assert.notEqual(code, 0, 'should return failure for linter');
+        helper.cleanup(done);
+      });
+    });
+
+    it('should run with no errors and --no-lint', function(done) {
+      var helper = new TestHelper({copyDist: true});
+
+      helper.exec(binName, ['--no-lint', '--no-build'], function(code, stdout, stderr) {
+
+        assert.equal(code, 0, 'should return success');
+        helper.cleanup(done);
+      });
+    });
+
+    it('should error on fake browser', function(done) {
+      var helper = new TestHelper({copyDist: true});
+
+      helper.exec(binName, ['--no-lint', '--no-build', '--browsers', 'cow'], function(code, stdout, stderr) {
+
+        assert.notEqual(code, 0, 'should return failure');
+        helper.cleanup(done);
+      });
+    });
+
+    it('should run just chrome', function(done) {
+      var helper = new TestHelper({copyDist: true});
+
+      helper.exec(binName, ['--no-lint', '--no-build', '--browsers', 'chrome'], function(code, stdout, stderr) {
 
         assert.equal(code, 0, 'should return success');
         helper.cleanup(done);
