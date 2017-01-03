79694c8483d7d66bfab02d5cfd5d00c8fd901cce
brandonocasey <brandonocasey@gmail.com>
2017-01-03 18:22:59 -0500

    fix rollup warning causing tests to fail

--- test/fixtures/test-pkg-main/test/index.test.js
+++ test/fixtures/test-pkg-main/test/index.test.js
@@ -3,12 +3,17 @@ import QUnit from 'qunitjs';
 import sinon from 'sinon';
 
 QUnit.module('test', {
-  beforeEach() {},
-  afterEach() {}
+  beforeEach() {
+    this.clock = sinon.useFakeTimers();
+  },
+  afterEach() {
+    this.clock.restore();
+  }
 });
 
 QUnit.test('foo fn returns bar', function(assert) {
   const test = new TestingThing();
+  const nope = 'not used';
 
   assert.strictEqual(test.foo(), 'bar', 'bar returns this.bar');
 });
